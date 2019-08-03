5f205d1e1d621bc930e9d64f61a076c661325b03
Chris <cjmarsh725@gmail.com>
2019-08-03 13:22:41 -0700

    Create item if id DNE or push entry to prices

--- backend/index.js
+++ backend/index.js
@@ -14,30 +14,20 @@ client.connect(err => {
     let data = [];
     axios.all(requests).then(response => {
       response.forEach(res => data = data.concat(res.data));
-      console.log(data.length);
+      data.forEach(item => {
+        const entry = {
+          date: new Date().toISOString(),
+          buys: item.buys || null,
+          sells: item.sells || null
+        };
+        collection.updateOne({id: item.id}, {$setOnInsert: {id: item.id, prices: []}}, {upsert: true})
+                  .then(result => {
+                  })
+                  .catch(error => console.log(error));
+        collection.updateOne({id: item.id}, {$push: {prices: entry}})
+                  .catch(error => console.log(error));
+      });
+      console.log(`Completed updating ${data.length} items`);
     }).catch(error => console.log(error));
-    // for (let i = 0; i < items.length; i++) {
-    //   collection.updateOne({id: items[i].id}, {$setOnInsert: items[i]}, {upsert: true})
-    //             .catch(error => console.log(error));
-    // }
-  }).catch(error => console.error(error));
-  // axios.get("https://api.guildwars2.com/v2/commerce/prices").then(response => {
-  //   const writeOps = [];
-  //   response.data.forEach(id => {
-  //     writeOps.push({ updateOne: {
-  //       "filter" : {"id" : id},
-  //       "update" : {$setOnInsert: {id: id, prices: []}},
-  //       "upsert" : true
-  //     }});
-  //   });
-  //   collection.bulkWrite(writeOps).then(result => {
-  //     console.log("BulkWrite completed for empty items");
-  //   })
-  //   // collection.updateOne({id: id}, {$setOnInsert: {id: id, prices: [], listings: []}}, {upsert: true})
-  //   // .catch(error => console.log(error));
-  // }).catch(error => console.error(error));
-  // axios.get("https://api.guildwars2.com/v2/commerce/prices?page=0&page_size=200").then(response => {
-  //   delete response.data;
-  //   console.log(response);
-  // }).catch(error => console.error(error));
+  }).catch(error => console.log(error));
 });
